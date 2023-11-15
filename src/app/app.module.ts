a4bd59d5c39288458efa4883a8a0fc47c4b7bce6
Varasiri Limprasert <bearish@odds.team>
2023-11-15 09:57:57 +0700

    create component get/post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormPostComponent } from './components/form-post/form-post.component';
+import { FormGetComponent } from './components/form-get/form-get.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormPostComponent,
+    FormGetComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/form-get/form-get.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-get',
+  templateUrl: './form-get.component.html',
+  styleUrl: './form-get.component.css'
+})
+export class FormGetComponent {
+
+}
